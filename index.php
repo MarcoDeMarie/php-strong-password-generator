1f4474723f1622d263c8f1399b44f02faed07a05
MarcoDeMarie <De.Marie.Marco50@gmail.com>
2023-05-18 17:11:25 +0200

    struttura

--- index.php
+++ index.php
@@ -4,9 +4,29 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <title>Document</title>
 </head>
 <body>
+
+<h1 class="d-flex justify-content-center">PASSWORD GENERATOR</h1>
+
+
+<div class="container d-flex justify-center my-5 col-6 ">
+
+  <p class="fs-4">Inserisci il numero di caratteri per la password</p>
+
+  <div class="lenght-counter col-2 ">
+
+    <div class="input-group mx-5">
+      <input type="number" class="form-control" aria-describedby="basic-addon1">
+    </div>
+
+  </div>
+</div>
+<div class="messaggio d-flex justify-content-center">
+    <p>messaggio</p>
+</div>
   
 </body>
 </html>
